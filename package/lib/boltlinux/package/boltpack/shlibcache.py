122292e9b28b5473603955841fcd79bb147b3ce3
Tobias Koch <tobias.koch@gmail.com>
2022-05-25 12:05:54 +0300

    shlibcache: overlay_package recognizes ld64* as possible SO dependency

--- package/lib/boltlinux/package/boltpack/shlibcache.py
+++ package/lib/boltlinux/package/boltpack/shlibcache.py
@@ -157,7 +157,7 @@ class ShlibCache:
         for src, attr in binary_package.contents.items():
             lib_name = os.path.basename(src)
 
-            if not re.match(r'^(?:lib|ld-).*?\.so.*$', lib_name):
+            if not re.match(r'^(?:lib|ld-|ld64).*?\.so.*$', lib_name):
                 continue
 
             if attr.stats.is_symbolic_link:
