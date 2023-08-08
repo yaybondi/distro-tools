fa6a41790255032b05fd26169452eaafccf4c066
Tobias Koch <tobias.koch@gmail.com>
2023-08-08 11:12:56 +0300

    environment.sh: initialize only this project

--- environment.sh
+++ environment.sh
@@ -1,6 +1,11 @@
-if [ ! -f environment.sh ] || [ ! -d distro-info  ]; then
-    echo "The environment.sh file must be sourced locally as '. environment.sh'."
-else
+BOLT_ENVIRONMENT_SH_PRE_FLIGHT_CHECKS_OK=true
+
+if [ ! -f "environment.sh" ]; then
+    echo "The environment.sh file must be sourced locally as '. ./environment.sh'."
+    BOLT_ENVIRONMENT_SH_PRE_FLIGHT_CHECKS_OK=false
+fi
+
+if [ "$BOLT_ENVIRONMENT_SH_PRE_FLIGHT_CHECKS_OK" = true ]; then
     mkdir -p .pythonpath/boltlinux
 
     ln -sf ../../ffi-libarchive/lib/boltlinux/ffi    .pythonpath/boltlinux/
@@ -11,19 +16,9 @@ else
     ln -sf ../../repository/lib/boltlinux/repository .pythonpath/boltlinux/
     ln -sf ../../image-gen/lib/boltlinux/osimage     .pythonpath/boltlinux/
 
-    if [ -d ../build-box-utils/lib/boltlinux/buildbox ]; then
-        ln -sf ../../../build-box-utils/lib/boltlinux/buildbox .pythonpath/boltlinux/
-    fi
-
-    if [ -d ../build-system/lib/boltlinux ]; then
-        ln -sf ../../../build-system/lib/boltlinux/packagedb .pythonpath/boltlinux/
-        ln -sf ../../../build-system/lib/boltlinux/builder   .pythonpath/boltlinux/
-        ln -sf ../../../build-system/lib/boltlinux/archive   .pythonpath/boltlinux/
-    fi
-
     touch .pythonpath/boltlinux/__init__.py
 
-    if [ "x$BOLT_DISTRO_TOOLS_SOURCED" = "x" ]; then
+    if [ "x$BOLT_LOCAL_PROJECT_SOURCED" = "x" ]; then
         export PYTHONPATH="$(pwd)/.pythonpath:$PYTHONPATH"
 
         PATH="$(pwd)/image-gen/bin:$PATH"
@@ -31,11 +26,10 @@ else
         PATH="$(pwd)/package/bin:$PATH"
         PATH="$(pwd)/repository/bin:$PATH"
 
-        PATH="$(pwd)/../build-system/bin:$PATH"
-        PATH="$(pwd)/../build-box-utils/bin:$PATH"
-
         export PATH
         export PS1="(distro-tools)$PS1"
-        export BOLT_DISTRO_TOOLS_SOURCED="yes"
+        export BOLT_LOCAL_PROJECT_SOURCED="yes"
     fi
 fi
+
+unset BOLT_ENVIRONMENT_SH_PRE_FLIGHT_CHECKS_OK
